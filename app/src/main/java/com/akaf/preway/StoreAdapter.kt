f43a6feb1e1a8eb839a5f2e4a6c9dacb246ec02f
mohammadreza <abbaszadeh@gmail.com>
2020-05-15 15:46:13 +0430

    created dialog for store items

--- app/src/main/java/com/akaf/preway/StoreAdapter.kt
+++ app/src/main/java/com/akaf/preway/StoreAdapter.kt
@@ -1,12 +1,15 @@
 package com.akaf.preway
 
 
+import android.app.AlertDialog
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.leader_board_lest_item.view.*
+import kotlinx.android.synthetic.main.store_raw_items.view.*
 
 class StoreAdapter(val context: Context, var productList: List<Int>) :
     RecyclerView.Adapter<StoreAdapter.ViewHolder>() {
@@ -29,10 +32,58 @@ class StoreAdapter(val context: Context, var productList: List<Int>) :
 
     class ViewHolder(val view: View, val context: Context) : RecyclerView.ViewHolder(view) {
 
+
+
         fun bind(image:Int){
+
+            setItemListener()
+
             with(view){
+
+
+
             }
         }
+
+
+        fun setItemListener()
+        {
+            view.store_items_first.setOnClickListener{
+                showDialog(context)
+
+            }
+
+            view.store_items_second.setOnClickListener{
+                showDialog(context)
+
+            }
+
+            view.store_items_third.setOnClickListener{
+                showDialog(context)
+
+            }
+        }
+
+
+        fun showDialog(context:Context){
+            val alertDialog = AlertDialog.Builder(context)
+
+            alertDialog.setView(R.layout.store_need_coin)
+            alertDialog.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+
+            })
+            alertDialog.show()
+        }
+
+
     }
 
 

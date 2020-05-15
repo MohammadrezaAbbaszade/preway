f64e499c7e0009d3ce03e7dbfa61861d8bcd7dd9
mohammadreza <abbaszadeh@gmail.com>
2020-05-15 15:12:19 +0430

    created dialog for legal and review us

---
+++ app/src/main/java/com/akaf/preway/HelpSheetDialogFragment.kt
@@ -0,0 +1,25 @@
+package com.akaf.preway
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class HelpSheetDialogFragment : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bottom_sheet_dialog, container, false)
+    }
+
+
+}
--- app/src/main/java/com/akaf/preway/MainFragment.kt
+++ app/src/main/java/com/akaf/preway/MainFragment.kt
@@ -1,12 +1,16 @@
 package com.akaf.preway
 
 
+import android.app.AlertDialog
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.footer_item.view.*
 import kotlinx.android.synthetic.main.fragment_main.view.*
 
@@ -24,28 +28,63 @@ class MainFragment : Fragment() {
             return fragment
         }
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        val view= inflater.inflate(R.layout.fragment_main, container, false)
+        val view = inflater.inflate(R.layout.fragment_main, container, false)
         val recyclerViewImages = ArrayList<Int>()
         recyclerViewImages.add(1)
-        initRecyclerView(recyclerViewImages,view)
+        initRecyclerView(recyclerViewImages, view)
 
         view.getHelpButton.setOnClickListener {
-            val intent= Intent(activity, HelpActivity::class.java)
+            val intent = Intent(activity, HelpActivity::class.java)
             startActivity(intent)
         }
+        view.legalButton.setOnClickListener {
+            val dialogView =
+                getLayoutInflater().inflate(R.layout.fragment_bottom_sheet_dialog, null)
+            val dialog = BottomSheetDialog(context!!)
+            dialog.setContentView(dialogView)
+            dialog.show()
 
+        }
+
+
+        view.reviewUsButton.setOnClickListener {
+
+            showDialog(context!!)
+        }
         return view
     }
 
-    private fun initRecyclerView(images: List<Int>,view:View) {
+    private fun initRecyclerView(images: List<Int>, view: View) {
         recyclerView =
             Adapter(context!!, images)
         view.main_hq_recycler.adapter = recyclerView
+
+    }
+
+
+    private fun showDialog(context: Context) {
+
+        val alertDialog = AlertDialog.Builder(context)
+
+        alertDialog.setView(R.layout.rate_us_flow)
+        alertDialog.setOnItemSelectedListener(object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+
+        })
+        alertDialog.show()
 
     }
 
